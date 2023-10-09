b7edac0602696c8e16efade58ec30cbf82c3ef7f
Liu-Steve <steve_liu@whu.edu.cn>
2023-10-09 18:02:52 +0000

    构建Spring Boot基本代码框架

---
+++ src/test/java/space/dragondj/go/GoApplicationTests.java
@@ -0,0 +1,13 @@
+package space.dragondj.go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
