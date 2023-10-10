3e4d82c1e386b872f4290df33338501b4ef75389
Liu-Steve <steve_liu@whu.edu.cn>
2023-10-10 17:17:06 +0000

    删去明文密码，优化部署

--- go-server/src/test/java/com/goplatform/server/goserver/GoServerApplicationTests.java
+++ go-server/src/test/java/com/goplatform/server/goserver/GoServerApplicationTests.java
@@ -1,13 +1,29 @@
 package com.goplatform.server.goserver;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class GoServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private StringEncryptor encryptor;
+
+    @Test
+    public void getPass() {
+        String name = encryptor.encrypt("name");
+        String password = encryptor.encrypt("password");
+        System.out.println("database name: " + name);
+        System.out.println("database password: " + password);
+        Assert.isTrue(name.length() > 0, "name is empty");
+        Assert.isTrue(password.length() > 0, "password is empty");
     }
 
 }
