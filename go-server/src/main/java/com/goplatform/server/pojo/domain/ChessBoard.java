f2febaf13dc546f77bf9d8020727c4c5fa3959c9
DragonDJ <1812296895@qq.com>
2023-10-11 01:31:19 +0800

    更新了棋盘和房间的属性

--- go-server/src/main/java/com/goplatform/server/pojo/domain/ChessBoard.java
+++ go-server/src/main/java/com/goplatform/server/pojo/domain/ChessBoard.java
@@ -5,4 +5,21 @@ import lombok.Data;
 @Data
 public class ChessBoard {
     //TODO 棋盘属性确定
+
+    // 创建用户ID
+    private Long createUserId;
+    // 创建用户名
+    private String createUserName;
+    // 白色方棋手
+    private Long whitePlayerId;
+    // 黑色方棋手
+    private Long blackPlayerId;
+    // 棋盘大小
+    private Long boardSize = 19L;
+    // 用于表示具体棋盘
+    private String[][] board;
+    // 棋盘状态 0：未开始 1：对局中 2：请求结束 3：结束（后续枚举类）
+    private Long status;
+    // 当前应该谁下棋 0：白色 1：黑色 （后续枚举类）
+    private Long nowPlayer;
 }
--- go-server/src/main/java/com/goplatform/server/pojo/domain/Room.java
+++ go-server/src/main/java/com/goplatform/server/pojo/domain/Room.java
@@ -2,7 +2,18 @@ package com.goplatform.server.pojo.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Room {
     // TODO 房间属性确定
+
+    // 创建用户Id
+    private Long createUserId;
+    // 创建用户名
+    private String createUserName;
+    // 棋盘
+    private ChessBoard chessBoard;
+    // 创建时间
+    private Date createdate;
 }
