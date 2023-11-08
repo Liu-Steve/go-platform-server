f05c131a0aaa96aa44987d3336df6f41c535590b
Liu-Steve <steve_liu@whu.edu.cn>
2023-11-08 10:07:48 +0000

    新建katago工程

---
+++ katago/src/main/java/com/goplatform/katago/katago/KatagoApplication.java
@@ -0,0 +1,13 @@
+package com.goplatform.katago.katago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatagoApplication.class, args);
+    }
+
+}
---
+++ katago/src/test/java/com/goplatform/katago/katago/KatagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goplatform.katago.katago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
